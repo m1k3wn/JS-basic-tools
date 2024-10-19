d278e95245952c9ba1f6c7ff79310216050bb2f5
m1k3wn <mike@mikewinnard.co.uk>
2024-10-19 08:30:21 +0100

    adds custom code snippets for VSCODE JS

---
+++ custom-Code-Snippets.js
@@ -0,0 +1,94 @@
+/* Javascript custom code snippets:
+Paste any/all into 'custom code snippets' into the javascript file on your VSCode and hit save
+Seperate each snippet with a comma as shown, NO COMMA after last snippet
+
+To use, type your set "prefix" in any javascript file and press tab,
+ press tab again to through $1, $2, $3 chosen cursor-positions. $0 is final cursor position
+
+Syntax: 
+
+"Name of Snippet" {
+    "prefix": "command you want to type to activate snippet",
+    "body": [ "code you want it to type" ] ($1, $2 etc set cursor positions, $0 sets final cursor position)
+    description: "brief summary of what snippet does"
+},
+
+1. Types console.log();
+2. Types console.log( , );
+3. Creates Arrow Function
+4. Creates For Lopp
+5. Creates for In Loop
+6. Creates For Of Loop
+
+*/
+
+// 1: console.log(x);(y) -  cursor tabs to (x) then (y) position.
+
+"Console Log Single": {
+    "prefix": "ll",
+    "body": [
+      "console.log($1);$0"
+    ],
+    "description": "Log a value to console with final cursor placement"
+},
+
+
+// 2: console.log( x, y); -  cursor tabs to (x) > (y) > (z) position.
+"Console Log Multi": {
+    "prefix": "lll",
+    "body": [
+      "console.log($1, $2);$0"
+    ],
+    "description": "Log two values to console with final cursor placement"
+},
+
+// 3: Arrow Function - const (x) = (y) => { z }; tabs to (x) > (y) > (z) position.
+"Arrow Function": {
+"prefix": "afn",
+"body": [
+  "const $1 = ($2) => {",
+  "  $3",
+  "};",
+  "$0"
+],
+"description": "Create an arrow function"
+},
+
+// 4 : For Loop - for (let (x) = 0; (x) < (y); (x)++) { (z) };  tabs to (x) > (y) > (z) position, auto-fills (x)s.
+"For Loop": {
+"prefix": "fl>>",
+"body": [
+  "for (let $1 = 0; $1 < $2; $1++) {",
+  "  $3",
+  "};",
+  "$0"
+],
+"description": "Basic For loop snippet"
+},
+
+// 5 : For In Loop: for (let (x) in (y)) { (z) };  tabs to (x) > (y) > (z) position.
+"For In Loop": {
+    "prefix": "fi>>",
+    "body": [
+      "for (let $1 in $2){",
+  "  $3",
+  "};",
+  "$0"
+    ],
+    "description": "For In loop snippet"
+},
+
+// 6 : For Of Loop - for (const (x) of (y)) { (z) };  tabs to (x) > (y) > (z) positions
+"For Of Loop": {
+"prefix": "fo>>",
+"body": [
+  "for (const $1 of $2){",
+"  $3",
+"};",
+"$0"
+],
+"description": "For Of Loop Snippet"
+}
+
+// 7 : Basic Test - 
+// will need three snippets, one for the require, one for run/skipTest, one for each check within test function
